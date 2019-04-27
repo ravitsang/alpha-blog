77a80a7edc94e4d6d165cc3e4685d02c7a12532d
Rails Udemy Course <14225425@life.hkbu.edu.hk>
2019-04-27 20:00:45 +0800

    style errors and messages partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,33 +3,33 @@ class ArticlesController < ApplicationController
 
 
   def index
-  	@articles = Article.all
+    @articles = Article.all
 
   end
   def new
-  	@article = Article.new
+    @article = Article.new
   end
 
   def edit
   end
 
   def create
-  	@article = Article.new(article_params)
-  	if @article.save
-  	  flash[:notice] = "Article was successfully created"
-  	  redirect_to article_path(@article) 
-  	else
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
       render 'new'
-  	end	
+    end
   end
 
   def update
-  	if @article.update(article_params)
-  	  flash[:notice] = "Article was successfully updated"
-  	  redirect_to article_path(@article) 
-  	else
+    if @article.update(article_params)
+      flash[:success] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
       render 'edit' #render to edit.html.erb
-  	end	
+    end
 
   end
 
@@ -37,18 +37,18 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-  	@article.destroy
-  	flash[:notice] = "Article was successfully deleted"
-  	redirect_to articles_path
+    @article.destroy
+    flash[:danger] = "Article was successfully deleted"
+    redirect_to articles_path
 
   end
-  
-  private
-    def set_article
-      @article = Article.find(params[:id])
-    end
 
-  	def article_params
-  	  params.require(:article).permit(:title, :description) # check the validation
-  	end
+  private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description) # check the validation
+  end
 end
