44c69bdb054ffc2b423173a9aca9fbb75e273d93
Rails Udemy Course <14225425@life.hkbu.edu.hk>
2019-05-06 01:58:26 +0800

    Add sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id # give the user id to session hash means the user signed in
       flash[:success] = "Welcome to the alpha blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new' #  the error will be show in the error partial when load the page again
     end
