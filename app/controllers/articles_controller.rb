4397dc3bef30f4dd2c602991e7dc31e6c8f36d09
Rails Udemy Course <14225425@life.hkbu.edu.hk>
2019-05-10 23:45:06 +0800

    Upadte all views with categories and create categories in articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -50,7 +50,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :description) # check the validation
+    params.require(:article).permit(:title, :description, category_ids: []) # check the validation
   end
 
   def require_same_user
